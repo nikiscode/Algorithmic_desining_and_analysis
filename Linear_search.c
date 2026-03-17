e5ea231164072c8f0ff0bf7173e887a64371f36b
Nikitha N <nikitha.nrb@gmail.com>
2026-03-17 22:19:25 +0530

    Update Linear_search.c

--- Linear_search.c
+++ Linear_search.c
@@ -1,5 +1,4 @@
 //linear search algorithm
-
 LinearSearch(A, n, key)
 1. Start
 2. Set i = 0
@@ -12,8 +11,7 @@ LinearSearch(A, n, key)
 4. If i == n
        Print "Element not found"
 5. Stop
-
-       ## Example
+  ## Example
 Array:
 A = [12, 25, 8, 32, 17]
 Key to search:
@@ -24,8 +22,7 @@ Step	Index (i)	Element	Comparison
 2	1	25	25 ≠ 32
 3	2	8	8 ≠ 32
 4	3	32	32 = 32       → Found
-
-       ##Result:
+  ##Result:
 Element 32 is found at index 3 (or position 4 in human counting).
 Time Complexity
 Best Case: O(1) (element found at first position)
