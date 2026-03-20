18cf7c261212bbfbbe1ac2828ded2fd2520b9cbd
Nikitha N <nikitha.nrb@gmail.com>
2026-03-20 21:51:29 +0530

    Update Random.c

--- Random.c
+++ Random.c
@@ -1,27 +1,20 @@
 //Random importion of the numbers
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 void Exch(int *p, int *q){
-   
-    int temp = *p;
+   int temp = *p;
     *p = *q;
     *q = temp;
 }
-
-void Quicksort(int a[], int low, int high){
-   
+void Quicksort(int a[], int low, int high){ 
     int i, j, key;
  if(low >= high)
         return;
-
     key = low;
     i = low + 1;
     j = high;
-
-  while(i <= j){
-       
+  while(i <= j){     
       while(i <= high && a[i] <= a[key])
             i++;
      while(a[j] > a[key])
@@ -29,7 +22,6 @@ void Quicksort(int a[], int low, int high){
          if(i < j)
             Exch(&a[i], &a[j]);
     }
-  
     Exch(&a[key], &a[j]);
     Quicksort(a, low, j - 1);
     Quicksort(a, j + 1, high);
@@ -39,18 +31,15 @@ int main(){
     clock_t start, end;
     printf("Enter number of elements: ");
     scanf("%d", &n);
-    if(n <= 20){
-       
+    if(n <= 20){ 
         printf("Enter %d elements:\n", n);
         for(i = 0; i < n; i++)
             scanf("%d", &a[i]);
     }
-    else{
-     
+    else{ 
         srand(time(0));
         for(i = 0; i < n; i++)
-            a[i] = rand() % 10000;
-
+            a[i] = rand() % 100000
         printf("\n%d random numbers generated.\n", n);
     }
     start = clock();
