7240593cac0910374c9f20587c0369c0dd9b8556
Nikitha N <nikitha.nrb@gmail.com>
2026-03-24 21:45:23 +0530

    Create MaxElement.c

---
+++ MaxElement.c
@@ -0,0 +1,9 @@
+ALGORITHM MaxElement(A[0…n-1])
+//Determines the value of the largest element in a given array
+//Input: An array A[0…n-1] of real numbers
+//Output: The value of the largest element in A
+maxval ← A[0]
+for i ← 1 to n-1 do
+if A[i] > maxval
+maxval ← A[i]
+return maxval
