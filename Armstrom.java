7f706a3aa2cb01dae5be9e623d38fa1045548041
Nikitha N <nikitha.nrb@gmail.com>
2026-04-29 22:44:09 +0530

    Update Armstrom.java

--- Armstrom.java
+++ Armstrom.java
@@ -1,9 +1,7 @@
 
-
 //Armstrom
 public class ArmstrongNumber {
-    
-    /**
+    **
      * Check if a number is an Armstrong number.
      * An Armstrong number is a number that is equal to the sum of its own digits
      * each raised to the power of the number of digits.
