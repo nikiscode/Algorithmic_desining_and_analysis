1043a9bf6aca4041175c9cc1d570a5d89249ec29
Nikitha N <nikitha.nrb@gmail.com>
2026-05-07 11:54:30 +0530

    Create NQueens_Algorithm.c

---
+++ NQueens_Algorithm.c
@@ -0,0 +1,74 @@
+7	Implement N Queen's problem using Back Tracking.
+N Queen’s problem is to place n queens on an n × n chessboard so that no two queens attack each other by being in the same row or in the same column or on the same diagonal. For n = 1, the problem has a trivial solution, and it is easy to see that there is no solution for n = 2 and n = 3.
+
+PROGRAM :
+
+#include<stdio.h> 
+#include<conio.h> 
+#include<math.h> 
+int a[30], count=0; 
+
+int place(int pos) 
+{ 
+int i; 
+for(i=1; i<pos; i++) 
+{ 
+if((a[i]==a[pos])||((abs(a[i] - a[pos])==abs(i - pos)))) 
+return 0; 
+} 
+return 1; 
+} 
+
+void print_sol(int n) 
+{ 
+int i, j; 
+count++; 
+printf("\n\nSolution #%d:\n",count); 
+for(i=1; i<=n; i++) 
+{ 
+for(j=1; j<=n; j++) 
+{ 
+if(a[i]==j) 
+printf("Q\t"); 
+else 
+printf("*\t"); 
+} 
+printf("\n"); 
+} 
+} 
+
+void queen(int n) 
+{ 
+int k=1; 
+a[k]=0; 
+while(k!=0) 
+{
+a[k]=a[k]+1; 
+while((a[k] <= n)&& !place(k)) 
+a[k]++; 
+if(a[k]<=n) 
+{ 
+if(k==n) 
+print_sol(n); 
+else 
+{ 
+k++; 
+a[k]=0; 
+} 
+} 
+else 
+k--; 
+} 
+} 
+
+void main() 
+{ 
+int i,n; 
+clrscr(); 
+printf("Enter the number of Queens\n"); 
+scanf("%d",&n); 
+queen(n); 
+printf("\nTotal solutions=%d",count); 
+getch()	; 
+}
+
